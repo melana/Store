fbecac7d2cda65841df4c839b216c9b00978e936
Oksana <oksan1ka@ukr.net>
2021-08-18 23:16:08 +0300

    first commit

---
+++ src/app/shared/product.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { FbResponse } from './interfaces';
+import { ProductComponent } from '../product/product.component';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private  http : HttpClient) { }
+
+  create(product:any) {
+    return this.http.post(`${environment.fbDbUrl}/products.json`, product)
+    .pipe(map( (res : FbResponse | any) => {
+      return {
+        ...product,
+        id: res.name,
+        date: new Date(product.date)
+      }
+    }))
+  }
+
+  getAll() {
+    return this.http.get(`${environment.fbDbUrl}/products.json`)
+    .pipe( map ( (res: any) => {
+      return Object.keys(res)    
+      .map( key => ({
+        ...res[key],
+        id: key,
+        date: new Date(res[key].date)
+      }))
+    
+    }))
+  
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyDiyLwZHFPkdddFRccYH1p95AbuX7VgUyE",
+  fbDbUrl: "https://online-store-7d55b-default-rtdb.europe-west1.firebasedatabase.app/",
+};
