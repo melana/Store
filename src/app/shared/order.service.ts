6e31eddcb97f3b57cb0cb9cfd5df21778f90891d
Oksana <oksan1ka@ukr.net>
2021-10-06 23:54:06 +0300

    order service

--- src/app/shared/order.service.ts
+++ src/app/shared/order.service.ts
@@ -22,4 +22,22 @@ export class OrderService {
     }))
   }
 
+  getAll() {
+    return this.http.get(`${environment.fbDbUrl}/orders.json`)
+    .pipe( map ( (res: any) => {
+      return Object.keys(res)
+      .map( key => ({
+        ...res[key],
+        id: key,
+        date: new Date(res[key].date)
+      }))
+    }))
+  }
+
+
+
+  remove (id: any) {
+    return this.http.delete(`${environment.fbDbUrl}/orders/${id}.json`)
+  }
+
 }
